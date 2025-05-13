2dd4869b7a3f1d0ea09ad65897b77b158362f266
AAMINABIBI <aaminabibi2324@gmail.com>
2025-05-13 16:01:07 +0500

    firebase cnfiguration

--- src/firebase.js
+++ src/firebase.js
@@ -3,21 +3,17 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
-// Your Firebase configuration (replace with your config from Firebase Console)
 const firebaseConfig = {
-    apiKey: "AIzaSyBxacw44fxmw64rw9bhOh37LrUTUPJM06k",
-    authDomain: "restart-cb3ce.firebaseapp.com",
-    projectId: "restart-cb3ce",
-    storageBucket: "restart-cb3ce.firebasestorage.app",
-    messagingSenderId: "73420420527",
-    appId: "1:73420420527:web:eb9918a28ccc443265ab5a",
-    measurementId: "G-KM1XN57LWX"
-  };
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId
+};
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Initialize services
 const auth = getAuth(app);
 const db = getFirestore(app);
 const storage = getStorage(app);
